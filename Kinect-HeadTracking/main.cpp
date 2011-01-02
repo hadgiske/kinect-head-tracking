0606b1ab703f58dedb05663b2e1c5a10ab559c61
Sp3ssi <Sp3ssi@17e18ffd-2d5c-bf65-2f45-b884abe9fe7f>
2011-01-02 22:15:05 +0000

    relative raumverschiebung mit maus

--- Kinect-HeadTracking/main.cpp
+++ Kinect-HeadTracking/main.cpp
@@ -46,12 +46,18 @@ public:
 		x = xk;
 		y = yk;
 		z = zk;
+		offset_x = 0;
+		offset_y = 0;
+		offset_z = 0;
 	}
 
 	WorldPos(){
 		x = 0;
 		y = 0;
 		z = 0;
+		offset_x = 0;
+		offset_y = 0;
+		offset_z = 0;
 	}
 
 	float x;
@@ -67,6 +73,20 @@ public:
 
 		return retval;
 	}
+
+	void SetOffset(float cx, float cy, float cz) {
+		x = x + (cx - offset_x);
+		offset_x=cx;
+
+		y = y + (cy - offset_y);
+		offset_y=cy;
+
+		z = z + (cz - offset_z);
+		offset_z=cz;
+	}
+private: 
+	float offset_x, offset_y, offset_z;
+
 };
 
 WorldPos wpos[4];
@@ -88,6 +108,13 @@ void init_wpos(){
 	wpos[3].y = +ROOM_Y/2;
 	wpos[3].z = -ROOM_Z/2;
 
+}
+
+void refresh_wpos(){
+	wpos[0].SetOffset(mouse.cx,mouse.cy,0);	
+	wpos[1].SetOffset(mouse.cx,mouse.cy,0);	
+	wpos[2].SetOffset(mouse.cx,mouse.cy,0);
+	wpos[3].SetOffset(mouse.cx,mouse.cy,0);
 }
 
 WorldPos getNormal(WorldPos first, WorldPos second){
@@ -243,126 +270,14 @@ void _stdcall skel_cal_end(xn::SkeletonCapability& skeleton, XnUserID user, XnBo
 	isCalStart=false;
 }
 
-float rot_angle=0;
-XnUInt16 pUserOld=0;
-void glut_display() {
-	xn::DepthMetaData pDepthMapMD;
-	xn::ImageMetaData pImageMapMD;
-	XnUserID pUser[2];
-	XnUInt16 nUsers=2;
-#ifdef DEBUGOUT
-	ofstream datei;
-#endif
 
-	pUser[0] = 0;
-	pUser[1] = 0;
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glLoadIdentity();
-	gluPerspective(45, WINDOW_SIZE_X/WINDOW_SIZE_Y, 1, 5000);
-//	glOrtho(0, WINDOW_SIZE_X, WINDOW_SIZE_Y, 0, -128, 128);
-
-/*	glMatrixMode(GL_TEXTURE);
-	glLoadIdentity();
-	glScalef(scalex, scaley, 1.0);
-	glTranslatef(transx/630, transy/480, 0.0);*/
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	// Warten auf neue Daten vom Tiefengenerator
-	//nRetVal = context.WaitAndUpdateAll();
-	checkError("Fehler beim Aktualisieren der Daten", nRetVal);
-
-	/* Anzahl der User auslesen und in Objekten speichern */
-	/*user.GetUsers(pUser, nUsers);
-	if(pUser[0]!=0 && pUserOld!=1) {
-		cout << "User 1 erkannt" << endl;
-		pUserOld=1;
-	}
-
-	xn::SkeletonCapability pSkeleton = user.GetSkeletonCap();
-	XnCallbackHandle hnd;
-	pSkeleton.RegisterCalibrationCallbacks(skel_cal_start, skel_cal_end, 0,hnd);
-	
-	if(calibration){
-		pSkeleton.RequestCalibration(pUser[0],true);
-		calibration = false;
-		cout << "Kalibration wird gestartet, bitte Arme im 90 Grad Winkel nach oben halten." << endl;
-	}
-
-	pSkeleton.SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
-	if(pSkeleton.IsCalibrated(pUser[0])) {
-		XnSkeletonJointTransformation head;
-		pSkeleton.StartTracking(pUser[0]);
-		pSkeleton.GetSkeletonJoint(pUser[0], XN_SKEL_HEAD, head);
-
-		if(head.position.fConfidence && head.orientation.fConfidence) {
-			headtrans.rotmat[0] = head.orientation.orientation.elements[0];
-			headtrans.rotmat[1] = head.orientation.orientation.elements[1];
-			headtrans.rotmat[2] = head.orientation.orientation.elements[2];
-			headtrans.rotmat[3] = 0;
-			
-			headtrans.rotmat[4] = head.orientation.orientation.elements[3];
-			headtrans.rotmat[5] = head.orientation.orientation.elements[4];
-			headtrans.rotmat[6] = head.orientation.orientation.elements[5];
-			headtrans.rotmat[7] = 0;
-			
-			headtrans.rotmat[8] = -head.orientation.orientation.elements[6];
-			headtrans.rotmat[9] = -head.orientation.orientation.elements[7];
-			headtrans.rotmat[10] =-head.orientation.orientation.elements[8];
-			headtrans.rotmat[11] = 0;
-
-			headtrans.rotmat[12] = 0;
-			headtrans.rotmat[13] = 0;
-			headtrans.rotmat[14] = 0;
-			headtrans.rotmat[15] = 1;
-
-			headtrans.x = head.position.position.X;
-			headtrans.y = head.position.position.Y;
-			headtrans.z = head.position.position.Z;
-			
-			 
-
-			clearScreen();
-			cout	<< "Confidence Position: " << head.position.fConfidence
-					<< " X: " << head.position.position.X
-					<< " Y: " << head.position.position.Y
-					<< " Z: " << head.position.position.Z << endl
-
-					<< "Confidence Rotation: " << head.orientation.fConfidence << endl
-					<< "\t" << headtrans.rotmat[0]
-					<< "\t" << headtrans.rotmat[4]
-					<< "\t" << headtrans.rotmat[8] << endl
-					<< "\t" << headtrans.rotmat[1]
-					<< "\t" << headtrans.rotmat[5]
-					<< "\t" << headtrans.rotmat[9] << endl
-					<< "\t" << headtrans.rotmat[2]
-					<< "\t" << headtrans.rotmat[6]
-					<< "\t" << headtrans.rotmat[10] << endl << endl;
-		}
-	}*/
-
-	//------------------------------------------------------------------------------------------
-	//BEGIN: Kamera-Test
-	//------------------------------------------------------------------------------------------
-	glTranslatef(0,0,-100);
-
-	glRotatef(mouse.cx,0,1,0);
-	glRotatef(-mouse.cy,1,0,0);
-
-
-
+void draw_room() {
 	glBegin(GL_QUADS);
 	glColor3f(1,1,1);
 	//Rückwand zeichen
 	//Normale berechnen
 	WorldPos normal = getNormal(wpos[1]-wpos[0],wpos[3]-wpos[0]);
 	glNormal3f(normal.x,normal.y,normal.z);
-
 	glVertex3f( wpos[0].x,wpos[0].y,wpos[0].z);
 	glVertex3f( wpos[1].x,wpos[1].y,wpos[1].z);
 	glVertex3f( wpos[2].x,wpos[2].y,wpos[2].z);
@@ -370,9 +285,7 @@ void glut_display() {
 	glEnd();
 
 	glColor3f(1,1,1);
-
 	glBegin(GL_QUADS);
-	
 	//Boden zeichen	
 	normal = getNormal(wpos[0]-wpos[1],WorldPos(ROOM_X/2,-ROOM_Y/2,ROOM_Z/2)-wpos[1]);
 	glNormal3f(normal.x,normal.y,normal.z);
@@ -391,10 +304,9 @@ void glut_display() {
 	glVertex3f(ROOM_X/2, ROOM_Y/2,ROOM_Z/2);
 	glVertex3f(wpos[2].x, wpos[2].y,wpos[2].z);
 	glVertex3f(wpos[1].x,wpos[1].y,wpos[1].z);
-
 	glEnd();
-	glBegin(GL_QUADS);
 
+	glBegin(GL_QUADS);
 	glColor3f(1,1,1);
 	//Linke Wand
 	normal = getNormal(wpos[3]-wpos[0], WorldPos(-ROOM_X/2,-ROOM_Y/2,ROOM_Z/2)-wpos[0]);
@@ -416,94 +328,46 @@ void glut_display() {
 	glVertex3f( wpos[2].x,wpos[2].y,wpos[2].z);
 	glVertex3f( ROOM_X/2,ROOM_Y/2,ROOM_Z/2);
 	glEnd();
+}
 
+float rot_angle=0;
+XnUInt16 pUserOld=0;
+void glut_display() {
+	xn::DepthMetaData pDepthMapMD;
+	xn::ImageMetaData pImageMapMD;
+	XnUserID pUser[2];
+	XnUInt16 nUsers=2;
+#ifdef DEBUGOUT
+	ofstream datei;
+#endif
+
+	pUser[0] = 0;
+	pUser[1] = 0;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluPerspective(45, WINDOW_SIZE_X/WINDOW_SIZE_Y, 1, 5000);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	refresh_wpos();
+
+	//------------------------------------------------------------------------------------------
+	//BEGIN: Kamera-Test
+	//------------------------------------------------------------------------------------------
+	glTranslatef(0,0,-110.4);
+
+	draw_room();
 
 	//glPushAttrib(GL_ALL_ATTRIB_BITS);
 	//glutSolidCube(5);
 	//glPopAttrib();
 	//------------------------------------------------------------------------------------------
 	
-
-/*	// Aktuelle Depth Metadaten auslesen
-	depth.GetMetaData(pDepthMapMD);
-	// Aktuelle Depthmap auslesen
-	const XnDepthPixel* pDepthMap = depth.GetDepthMap();
-
-	if(maxdepth==-1)
-		maxdepth = getMaxDepth(pDepthMap);
-
-	// Aktuelle Image Metadaten auslesen 
-	image.GetMetaData(pImageMapMD);
-	//Aktuelles Bild auslesen
-	const XnRGB24Pixel* pImageMap = image.GetRGB24ImageMap();
-
-	glColor3f(1, 1, 1);
-//	XnDepthPixel maxdepth = depth.GetDeviceMaxDepth();
-	const unsigned int xres = pDepthMapMD.XRes();
-	const unsigned int yres = pDepthMapMD.YRes();
-
-#ifdef DEBUGOUT
-	if(printFile)
-		datei.open("daniel.txt", ios::out);
-#endif
-
-
-	for(unsigned int y=0; y<yres-1; y++) {
-		for(unsigned int x=0; x<xres; x++) {
-			aDepthMap[x+y*xres] = static_cast<GLubyte>(static_cast<float>(pDepthMap[x+y*xres])/static_cast<float>(maxdepth)*255);
-		}
-	}
-	
-
-	
-	glEnable(GL_TEXTURE_2D);
-	glPushMatrix();
-	glLoadIdentity();
-	glTranslatef(-800, 0, -2000);
-	glBindTexture(GL_TEXTURE_2D, texture_rgb);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, pImageMap);
-	glBegin(GL_QUADS);
-		glTexCoord2f(0,1); glVertex3f(0,0,0);
-		glTexCoord2f(1,1); glVertex3f(640,0,0);
-		glTexCoord2f(1,0); glVertex3f(640,480,0);
-		glTexCoord2f(0,0); glVertex3f(0,480,0);
-	glEnd();
-	glPopMatrix();
-
-	glPushMatrix();
-	glLoadIdentity();
-	glTranslatef(-800, 600, -2000);
-	glBindTexture(GL_TEXTURE_2D, texture_depth);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE8, 640, 480, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, aDepthMap);
-	glBegin(GL_QUADS);
-		glTexCoord2f(0,1); glVertex3f(0,0,0);
-		glTexCoord2f(1,1); glVertex3f(640,0,0);
-		glTexCoord2f(1,0); glVertex3f(640,480,0);
-		glTexCoord2f(0,0); glVertex3f(0,480,0);
-	glEnd();
-	glPopMatrix();*/
-/*
-	glPushMatrix();
-	glLoadIdentity();
-	glTranslatef(-100, -100, -2000);
-	glRotatef(cx,0,1,0);
-	glRotatef(cy,1,0,0);
-	glTranslatef(-320, -240, 1000);
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, texture_rgb);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, pImageMap);
-	glBegin(GL_POINTS);
-	for(unsigned int y=0; y<yres-1; y++) {
-		for(unsigned int x=0; x<630; x++) {
-			if(pDepthMap[x+y*xres]!=0) {
-				glTexCoord2f(static_cast<float>(x)/static_cast<float>(630), static_cast<float>(y)/static_cast<float>(480)); 
-				glVertex3f(x, (yres-y), -pDepthMap[x+y*xres]/2.00);
-			}
-		}
-	}
-	glEnd();
-	glPopMatrix();*/
-
 	glDisable(GL_TEXTURE_2D);
 	glutSwapBuffers();
 #ifdef DEBUGOUT
@@ -515,48 +379,6 @@ void glut_display() {
 }
 
 int main(int argc, char **argv) {
-	//nRetVal = XN_STATUS_OK;
-
-	///* Context initialisieren (Kameradaten) */
-	//nRetVal = context.Init();
-	//checkError("Fehler beim Initialisieren des Context", nRetVal)?0:exit(-1);
-
-
-
-	///* Tiefengenerator erstellen */
-	//nRetVal = depth.Create(context);
-	//checkError("Fehler beim Erstellen des Tiefengenerators", nRetVal)?0:exit(-1);
-
-	///* Tiefengenerator einstellen */
-	//XnMapOutputMode outputModeDepth;
-	//outputModeDepth.nXRes = 640;
-	//outputModeDepth.nYRes = 480;
-	//outputModeDepth.nFPS = 30;
-	//nRetVal = depth.SetMapOutputMode(outputModeDepth);
-	//checkError("Fehler beim Konfigurieren des Tiefengenerators", nRetVal)?0:exit(-1);
-
-
-	///* Imagegenerator erstellen */
-	//nRetVal = image.Create(context);
-	//checkError("Fehler beim Erstellen des Bildgenerators", nRetVal)?0:exit(-1);
-
-	///* Imagegenerator einstellen */
-	//XnMapOutputMode outputModeImage;
-	//outputModeImage.nXRes = 640;
-	//outputModeImage.nYRes = 480;
-	//outputModeImage.nFPS = 30;
-	//nRetVal = image.SetMapOutputMode(outputModeImage);
-	//checkError("Fehler beim Konfigurieren des Bildgenerators", nRetVal)?0:exit(-1);	
-
-	///* SceneAnalzer einstellen */
-	//nRetVal = user.Create(context);
-	//checkError("Fehler beim Konfigurieren des Usergenerators", nRetVal)?0:exit(-1);
-
-
-	///* Starten der Generatoren - volle Kraft vorraus! */
-	//nRetVal = context.StartGeneratingAll();
-	//checkError("Fehler beim Starten der Generatoren", nRetVal)?0:exit(-1);
-
 	/* Glut initialisieren */
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
@@ -569,11 +391,7 @@ int main(int argc, char **argv) {
 
 
 	float light_position[4] = {0.0,0.0,0.0,1.0};
-
 	glLightfv(GL_LIGHT0,GL_POSITION,light_position);
-
-	//glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.5);
-	
 	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.003);
 	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.0002);
 
@@ -587,24 +405,6 @@ int main(int argc, char **argv) {
 
 
 	//glEnable(GL_CULL_FACE);           //Backface Culling aktivieren
-//	glEnable(GL_ALPHA_TEST);
-//	glAlphaFunc(GL_GEQUAL, 1);
-
-	/* Texturen */
-	glGenTextures(1, &texture_rgb);
-	glBindTexture(GL_TEXTURE_2D, texture_rgb);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-
-	glGenTextures(1, &texture_depth);
-	glBindTexture(GL_TEXTURE_2D, texture_depth);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
 	glutDisplayFunc(glut_display);
 	glutIdleFunc(glut_idle);
